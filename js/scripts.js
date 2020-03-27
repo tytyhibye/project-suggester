26de7d52a0db2224a26d843e930c5b2fc2e403a2
Tyler Bates <Fondlytyler@gmail.com>
2020-03-27 10:00:35 -0700

    add var definitions for survey answers and create prompt for any unanswered questions.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(#function() {
+  $("form#formGroup").submit(function(event) {
+    event.preventDefault();
+    var dish = $("input:radio[name=dish]:checked").val();
+    var book = $("input:radio[name=book]:checked").val();
+    var car = $("input:radio[name=car]:checked").val();
+    var power = $("input:radio[name=power]:checked").val();
+    var free = $("input:radio[name=free]:checked").val();
+    if(dish === undefined || book === undefined || car === undefined || power === undefined || free === undefined) {
+      alert('Please answer all of the questions');
+     } else if()
+  });
+});
