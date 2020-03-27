4bcf32d34b7ca8c1f305d8cc1e3359369349bda9
Tyler Bates <Fondlytyler@gmail.com>
2020-03-27 10:15:10 -0700

    link questions to c# result

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(#function() {
+$(document).ready(function() {
   $("form#formGroup").submit(function(event) {
     event.preventDefault();
     var dish = $("input:radio[name=dish]:checked").val();
@@ -8,6 +8,10 @@ $(document).ready(#function() {
     var free = $("input:radio[name=free]:checked").val();
     if(dish === undefined || book === undefined || car === undefined || power === undefined || free === undefined) {
       alert('Please answer all of the questions');
-     } else if()
+    } else if(dish === 'clean' && book === 'long' && car === 'looks' && power === 'fly' && free === 'game') {
+      $('#js').hide();
+      $('#python').hide();
+      $('#csharp').show();
+    }
   });
 });
